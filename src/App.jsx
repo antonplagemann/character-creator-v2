19c856e69df7bf2479d03e0b086edb4ad02e4254
Anton Plagemann <anton@studicon.com>
2025-04-07 14:42:25 +0000

    finish exercise

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import BottomBackground from './components/BottomBackground';
 
 function App() {
   return (
     <>
+      <BottomBackground />
       <CharacterEditor />
       <Footer />
     </>
